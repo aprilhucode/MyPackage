cafdfa4c9b7052d62d80f2ca7c6b9f66eb0072f2
April Huang <phuang@beaconfireinc.com>
2024-01-23 13:22:26 -0500

    Create color grid

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MyPackage",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,14 +17,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/mojzesh/swift-colorful.git", from: "1.0.0")
+        .package(url: "https://github.com/mojzesh/swift-colorful.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyPackage",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Colorful", package: "swift-colorful")
+            ]),
         .testTarget(
             name: "MyPackageTests",
             dependencies: ["MyPackage"]),
